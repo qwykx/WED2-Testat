5bf93075ce4d22087c590384cb0cec3af90d805e
Alexander Steiner <a.steiner@hotmail.de>
2019-09-27 15:39:18 +0200

    Fixed Server now working on port 3000

--- NotesApplication/app.js
+++ NotesApplication/app.js
@@ -4,7 +4,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
-const routes = require('./routes/index');
+//const routes = require('./routes/index');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -14,4 +14,6 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
+
+module.exports = app;
