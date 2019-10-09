ca5c4816c6b997bcc70edf18fe2c3e9dd7107aa7
qwykx <vonkaenel.sacha@gmail.com>
2019-10-09 13:51:46 +0200

    Update app.js

--- NotesApplication/app.js
+++ NotesApplication/app.js
@@ -1,22 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import hbs from 'express-hbs';
+import path from 'path';
+import {noteRoutes} from './routes/noteRoutes';
 
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
-const hbs = require('hbs');
-const app = express();
-const routes = require('./routes/noteRoutes');
-
-
-app.set('views', path.join(__dirname, 'views'));
+export const app = express();
+app.engine('hbs', hbs.express4());
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(routes);
-
-app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(noteRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
-
-module.exports = app;
