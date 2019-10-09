5b6b42d7c79e5ec44168c7566fd901f6ae0a5bc8
qwykx <vonkaenel.sacha@gmail.com>
2019-10-09 14:53:39 +0200

    fix other crap

--- NotesApplication/controllers/noteController.js
+++ NotesApplication/controllers/noteController.js
@@ -4,15 +4,15 @@ export class NoteController {
 
     showIndex(req, res) {
         res.render("index");
-    }
+    };
 
     showCreate(req, res) {
         res.render("createNote");
-    }
+    };
 
     async createNote(req, res){
         let note = new noteService.Note(req.body.title, req.body.beschreibung, req.body.wichtigkeit, req.body.fertigBis, req.body.erledigt);
         await res.render("index", await noteStore.add(note));
-    }
+    };
 }
 export const noteController = new NoteController();
--- NotesApplication/routes/noteRoutes.js
+++ NotesApplication/routes/noteRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import {noteController} from '../controllers/noteController.js';
 
-router.get("/", noteController.index);
+router.get("/", noteController.showIndex.bind(noteController));
 router.get("/create", noteController.showCreate.bind(noteController));
 router.post("/create", noteController.createNote.bind(noteController));
 
