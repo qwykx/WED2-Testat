19db8279bdddaa1a05340aa19c344e285d3a81e9
Alexander Steiner <a.steiner@hotmail.de>
2019-10-09 10:08:32 +0200

    Db working -> fixed with callback

--- NotesApplication/controllers/NoteController.js
+++ NotesApplication/controllers/NoteController.js
@@ -1,7 +1,17 @@
+
 const qs = require('qs');
+const noteService = require('../services/noteService');
 
 
 function index(req, res){
+
+   const note = new noteService.Note("test","test",5,"21.08.2019",true);
+   const service = new noteService.Service();
+   service.addNoteToDb(note, ()=>{
+       console.log("succes");
+   });
+   service.getAllNotes(function (err,docs){ console.log(docs)
+   });
     res.render('index');
 }
 function showCreate(req, res){
