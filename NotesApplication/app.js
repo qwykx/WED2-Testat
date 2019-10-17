a327bdb099c27e56889a47a110a9e086c5441a2c
qwykx <vonkaenel.sacha@gmail.com>
2019-10-17 09:43:44 +0200

    Handlebars setup, added css

--- NotesApplication/app.js
+++ NotesApplication/app.js
@@ -5,9 +5,12 @@ import path from 'path';
 import {noteRoutes} from './routes/noteRoutes';
 
 const app = express();
-app.engine('hbs', hbs.express4());
+app.engine('hbs', hbs.express4({
+    defaultLayout: 'views/layouts/default',
+    layoutsDir: path.resolve('views/layouts/'),
+    partialsDir: path.resolve('views/partials/')
+}));
 app.set('view engine', 'hbs');
-app.set('views', path.resolve('views'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
