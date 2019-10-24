c627e1378cb9edcc476e5970680561f75f8f24ea
qwykx <vonkaenel.sacha@gmail.com>
2019-10-24 11:05:16 +0200

    Refactor routes

--- NotesApplication/routes/noteRoutes.js
+++ NotesApplication/routes/noteRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 import {noteController} from '../controllers/noteController.js';
 
 router.get("/", noteController.showIndex.bind(noteController));
-router.get("/notes", noteController.showCreate.bind(noteController));
-router.post("/notes", noteController.createNote.bind(noteController));
-router.get("/notes/edit/:id/", noteController.editNote.bind(noteController));
-router.post("/notes/edit/:id/", noteController.updateNote.bind(noteController));
-router.post("/notes/delete/:id/", noteController.deleteNote.bind(noteController));
+router.get("/new", noteController.showCreate.bind(noteController));
+router.post("/new", noteController.createNote.bind(noteController));
+router.get("/edit/:id/", noteController.editNote.bind(noteController));
+router.post("/edit/:id/", noteController.updateNote.bind(noteController));
+router.post("/delete/:id/", noteController.deleteNote.bind(noteController));
 
 //sorting routes
 router.get("/sortedByDate", noteController.showSortedByDate.bind(noteController));
